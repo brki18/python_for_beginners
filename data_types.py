87c157e36ca3abd9e848c3abf062635f1fe08378
Marko Brkusanin <marko.brkusanin@hpe.com>
2023-04-18 09:35:19 +0200

    initial commit

---
+++ data_types.py
@@ -0,0 +1,38 @@
+number = -3.14987
+precedence = ((3 + 2) * 3)
+# number = "text"
+# This is comment
+# print(number)
+# print(type(number))
+
+# Operators
+# +, -, *, /, **, %
+# print(round(number,2))
+# print(abs(number))
+# print(type(number))
+
+# Precedence
+# Order
+# ()
+# **
+# * /
+# + -
+# print(precedence)
+letter = "s"
+_letter = "a"
+letter12 = ""
+Letter = 'uppercase'
+camelCaseLetter = 'works'
+snake_case = '''
+snake
+multiline
+'''
+print(snake_case)
+# print(letter)
+'''
+asdadsa
+'''
+# print(Letter)
+# False = 12
+PI = 3.14
+__asd = 123
