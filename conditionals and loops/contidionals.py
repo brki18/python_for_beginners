73ae0c651f5820e0b6190d17e21eeed24e230319
Marko Brkusanin <marko.brkusanin@hpe.com>
2023-05-03 10:09:38 +0200

    conditionals and loops

---
+++ conditionals and loops/contidionals.py
@@ -0,0 +1,59 @@
+is_licenced = False
+is_production = True
+is_expired = False
+
+# and or
+
+# if is_licenced:
+#     print('Licence is valid')
+#     if is_production:
+#         print('Environment is production')
+# elif is_expired:
+#     print('Licence expired')
+# else:
+#     print('Licence is not valid but not expired')
+#
+# print('Outside of if block')
+
+# Truthy and Falsy
+
+# print(bool('hello'))
+# print(bool(''))
+# print(bool(5))
+# print(bool(0))
+
+# truthy_list = []
+#
+# if truthy_list:
+#     print(truthy_list)
+# else:
+#     print('List is empty')
+
+# Ternary operator
+# text = 'Software is licenced' if is_licenced else 'Software is not licenced'
+#
+# print(text)
+
+# Logical operators
+
+# > < == >= <= !=
+
+# print(4 < 7)
+# print(41 < 7)
+# print(4 == 4)
+# print(4 <= 7)
+# print(4 >= 7)
+
+# print([] == 0)
+# is ==
+list1 = [1, 2, 3]
+list2 = [1, 2, 3]
+# is vs ==
+# print(list1 == list2)
+# print(list1 is list2)
+
+text1 = 'hello'
+text2 = 'hello'
+
+print(text1 == text2)
+print(text1 is text2)
---
+++ conditionals and loops/loops.py
@@ -0,0 +1,58 @@
+text = 'This is python for beginners course'
+list_of_numbers = [1, 2, 3]
+
+# for char in text:
+#     print(char)
+#     for number in list_of_numbers:
+#         print(number)
+set1 = {'a', 'b'}
+# for item in set1:
+#     print(item)
+
+config_file = {
+    'username': 'Marko',
+    'password': 'secret',
+    'is_admin': True,
+    'email': 'email@gmail.com'
+}
+
+# for key, value in config_file.items():
+#     print(f'Key is: {key} and value is: {value}')
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+# for i in range(10, 2, -2):
+#     print(i)
+
+# for index, char in enumerate(text):
+#     print(f'Character: {char} is at index: {index}')
+
+# while loops
+
+while_condition = 1
+
+# while while_condition < 20:
+#     print(f"{while_condition} is less than 20")
+#     while_condition += 1
+# else:
+#     print('exited the while')
+
+# break, continue, pass
+
+# while while_condition < 20:
+#     print(f"{while_condition} is less than 20")
+#     while_condition += 1
+#     break
+# else:
+#     print('exited the while')
+
+# while while_condition < 20:
+#     print(f"{while_condition} is less than 20")
+#     continue
+#     while_condition += 1
+# else:
+#     print('exited the while')
+
+while while_condition < 20:
+    pass
